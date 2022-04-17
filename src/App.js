6a5dc28b00db80d70252262fb3845ff7558c5d24
IsaiVids <isaivids2020@gmail.com>
2022-04-17 21:46:22 +0530

    commit

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect} from 'react';
+import React, { useState} from 'react';
 import './App.css';
 
 function App() {
@@ -64,11 +64,4 @@ function App() {
 }
 
 export default App;
-
-  // return (
-  //   <div className={ !theme ? 'light' : 'dark'}>
-  //     <p>Hello Calc</p>
-  //     <button onClick={handleTheme}>Click me</button>
-  //   </div>
-  // )
 
